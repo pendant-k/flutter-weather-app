a3e4ea59d84e693f611b39624cb9fc9ec706489e
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-11-23 13:56:51 +0900

    :heavy_plus_sign: Add after first layout, lottie packages

--- lib/view/s_main.dart
+++ lib/view/s_main.dart
@@ -13,6 +13,28 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       body: Container(
         width: double.infinity,
+        child: RefreshIndicator(
+          onRefresh: () async {},
+          child: FutureBuilder(
+              future: null,
+              builder: (context, snapshot) {
+                // if (snapshot.hasError)
+                return const Center(child: Text('Error'));
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      strokeWidth: 4,
+                    ),
+                  );
+                return SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Text('hellow'),
+                    ],
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
